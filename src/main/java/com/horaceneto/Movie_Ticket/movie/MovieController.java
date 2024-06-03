a87fb2770165c6ad32faeaf6f76a158f792dac46
Fiasco <horaciomota93@gmail.com>
2024-06-03 18:17:19 +0100

    fix: AddMovie removed and now new mwthod (CreateMovie) replaced

--- src/main/java/com/horaceneto/Movie_Ticket/movie/MovieController.java
+++ src/main/java/com/horaceneto/Movie_Ticket/movie/MovieController.java
@@ -1,4 +1,7 @@
 package com.horaceneto.Movie_Ticket.movie;
+import com.horaceneto.Movie_Ticket.cinema.Screen;
+import com.horaceneto.Movie_Ticket.cinema.ScreenRepository;
+import com.horaceneto.Movie_Ticket.cinema.ScreenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -6,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api/movie")
@@ -16,7 +20,9 @@ public class MovieController {
 
     @PostMapping("/create")
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
-        Movie newMovie = movieService.addMovie(movie);
-        return new ResponseEntity<>(newMovie, HttpStatus. CREATED);
+        Movie newMovie = movieService.createMovie(movie);
+        return new ResponseEntity<>(newMovie, HttpStatus.CREATED);
     }
+
+
 }
