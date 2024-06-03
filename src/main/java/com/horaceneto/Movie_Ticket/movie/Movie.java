95292334198aed783bdaf9b64d8a73fe50059a4d
Fiasco <horaciomota93@gmail.com>
2024-06-03 03:19:15 +0100

    feat: Create movie class + dto

---
+++ src/main/java/com/horaceneto/Movie_Ticket/movie/Movie.java
@@ -0,0 +1,25 @@
+package com.horaceneto.Movie_Ticket.movie;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String Title;
+
+    private String sinopse;
+    private String cinema;
+
+}
