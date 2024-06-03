a3f66db4fd243d4a9a82c47a02a70542a83e6c84
Fiasco <horaciomota93@gmail.com>
2024-06-03 03:20:10 +0100

    feat: Movie controller working, first endingpoint working

---
+++ src/main/java/com/horaceneto/Movie_Ticket/movie/MoviceController.java
@@ -0,0 +1,22 @@
+package com.horaceneto.Movie_Ticket.movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/movie")
+public class MoviceController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie newMovie = movieService.addMovie(movie);
+        return new ResponseEntity<>(newMovie, HttpStatus. CREATED);
+    }
+}
